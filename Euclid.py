e138ca8fa53127ef26057911d984c012ac242852
BellyTheFairy <daniel.edu00k@gmail.com>
2025-12-04 20:09:04 +0300

    Euclid.py

--- Euclid.py
+++ Euclid.py
@@ -1,15 +1,20 @@
-inp = input('Введите два числа ')
-inp_is = inp.split()
-inp_1 = int(inp_is[0])
-inp_2 = int(inp_is[1])
-
-def gcd(a:int, b:int) -> int:
-    if a == 0 or b == 0: 
-         return max(a, b)
-    else:
-        if a > b:
-            return gcd(a - b, b)
-        else:
-            return gcd(a, b - a)
+def GCD(a,b):
+    
+    if b == 0:
+        print('НОД чисел',a_answer,'и',b_answer,'=',a)
         
-print(f'Нод чисел {inp_1} и {inp_2}: {gcd(inp_1, inp_2)}')
+    else:
+        c=a
+        a=b
+        b=c%b
+        return GCD(a,b)
+        
+print('Введите два числа, для нахождения их НОД')
+
+inpt=input()
+inpt=inpt.split()
+a=int(inpt[0])
+b=int(inpt[1])
+a_answer=a
+b_answer=b
+d=GCD(a,b)
